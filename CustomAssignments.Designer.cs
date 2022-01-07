29834783205cc7da6c5add8186a59aa76556b093
dsdguy <dsdguy100@gmail.com>
2022-01-07 14:46:58 -0600

    Add project files.

---
+++ CustomAssignments.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace PartC
+{
+    partial class CustomAssignments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.AssignmentTitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DueDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HourDue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.AssignmentTitle,
+            this.DueDate,
+            this.HourDue});
+            this.dataGridView1.Location = new System.Drawing.Point(174, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(452, 348);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // AssignmentTitle
+            // 
+            this.AssignmentTitle.HeaderText = "Assignment Name";
+            this.AssignmentTitle.Name = "AssignmentTitle";
+            this.AssignmentTitle.Width = 210;
+            // 
+            // DueDate
+            // 
+            this.DueDate.HeaderText = "DueDate";
+            this.DueDate.Name = "DueDate";
+            // 
+            // HourDue
+            // 
+            this.HourDue.HeaderText = "Hour Due";
+            this.HourDue.Name = "HourDue";
+            // 
+            // CustomAssignments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "CustomAssignments";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.CustomAssignments_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AssignmentTitle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DueDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HourDue;
+    }
+}
---
+++ HomePage.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace PartC
+{
+    partial class Homepage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHWTracker = new System.Windows.Forms.Label();
+            this.AddCustom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHWTracker
+            // 
+            this.lblHWTracker.AutoSize = true;
+            this.lblHWTracker.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHWTracker.Location = new System.Drawing.Point(258, 42);
+            this.lblHWTracker.Name = "lblHWTracker";
+            this.lblHWTracker.Size = new System.Drawing.Size(264, 36);
+            this.lblHWTracker.TabIndex = 0;
+            this.lblHWTracker.Text = "Homework Tracker";
+            // 
+            // AddCustom
+            // 
+            this.AddCustom.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddCustom.Location = new System.Drawing.Point(433, 144);
+            this.AddCustom.Name = "AddCustom";
+            this.AddCustom.Size = new System.Drawing.Size(243, 65);
+            this.AddCustom.TabIndex = 1;
+            this.AddCustom.Text = "Insert Custom Assignments";
+            this.AddCustom.UseVisualStyleBackColor = true;
+            this.AddCustom.Click += new System.EventHandler(this.AddCustom_Click);
+            // 
+            // Homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AddCustom);
+            this.Controls.Add(this.lblHWTracker);
+            this.Name = "Homepage";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHWTracker;
+        private System.Windows.Forms.Button AddCustom;
+    }
+}
+
